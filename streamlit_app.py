82fa127c79f355886254fecf98cfd485d30c4144
Sergey Mochalov <proweb@users.noreply.github.com>
2025-06-11 21:05:42 +0000

    Update output

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from urllib3.exceptions import HTTPError
 # Disable SSL warnings (not recommended for production)
 urllib3.disable_warnings()
 
+@st.cache_data
 def fetch_url_content(url):
     """Fetch content from a given URL"""
     http = urllib3.PoolManager()
@@ -24,7 +25,7 @@ st.title("URL Content Fetcher")
 
 # Create form
 with st.form("url_form"):
-    url = st.text_input("Enter URL:", placeholder="https://example.com", type="default")
+    url = st.text_input("Enter URL:",value=None, placeholder="https://example.com", type="default")
     submit_button = st.form_submit_button("Fetch Content")
 
 # When form is submitted
@@ -33,8 +34,20 @@ if submit_button and url:
     
     with st.spinner("Loading..."):
         content = fetch_url_content(url)
+
+    with st.expander("Source code"):
+        st.caption(":blue[HTML code]")
+        st.code(content, language='html', line_numbers=True)
+    # Тут будут список CSS
+    with st.expander("CSS Links"):
+        st.write(''' **Тут ссылки на CSS из документа**''')
+    with st.expander("JS Links"):
+        st.write(''' **Тут ссылки на JS из документа**''')
     
-    st.subheader("Page Content:")
-    st.code(content, language='html')
+    
+    
+  
+    
+    
 elif submit_button and not url:
-    st.warning("Please enter a URL")
+    st.warning("Please enter a URL!")
